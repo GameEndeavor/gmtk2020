01480c946f18501ce8921cb8118de42dd170a6e9
Richard Pittman <rk3OmegaDev@gmail.com>
2020-07-12 11:29:27 -0400

    scene changer

--- Game.gd
+++ Game.gd
@@ -1,4 +1,35 @@
 extends Node2D
 
+const LEVELS = [
+	preload("res://world/level/levels/BreakingFree.tscn"),
+	preload("res://world/level/levels/Victory.tscn"),
+]
+
+onready var transition_animator = $HUDLayer/Interface/TransitionEffects/TransitionAnimator
+
+var level = null
+var level_idx = 0
+
 func _ready():
-	Globals.level = self
+	Globals.game = self
+	load_level(0)
+
+func next_level():
+	var idx = level_idx + 1
+	if idx < LEVELS.size():
+		load_level(idx)
+
+func load_level(idx):
+	transition_animator.play("fade_black")
+	yield(transition_animator, "animation_finished")
+	
+	if Globals.level != null:
+		Globals.level.queue_free()
+		yield(get_tree(), "physics_frame")
+	
+	Globals.level = LEVELS[idx].instance()
+	level_idx = idx
+	add_child(Globals.level)
+	
+	transition_animator.play_backwards("fade_black")
+	yield(transition_animator, "animation_finished")
--- Globals.gd
+++ Globals.gd
@@ -4,10 +4,13 @@ const UNIT_SIZE = 16
 const PLAYER_JUMP_HEIGHT = -3.75 * UNIT_SIZE
 const PLAYER_JUMP_DURATION = 0.35
 
+
 var gravity = null
 var player = null
+var game = null
 var level = null
 var time = 0
+var level_idx = 0
 
 func _ready():
 	gravity = -2 * PLAYER_JUMP_HEIGHT / pow(PLAYER_JUMP_DURATION, 2)
